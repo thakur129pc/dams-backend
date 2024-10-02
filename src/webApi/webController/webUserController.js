8789776adfd00d0329e78c8c80048bb86f148c00
unknown <thakur129pc@gmail.com>
2024-10-03 00:58:51 +0530

    0001

--- src/webApi/webController/webUserController.js
+++ src/webApi/webController/webUserController.js
@@ -1,9 +1,12 @@
-import jwt from 'jsonwebtoken';
-import User from '../webModel/webUserDetails.js'; // Ensure the path is correct
+import jwt from "jsonwebtoken";
+import User from "../webModel/webUserDetails.js";
+import bcrypt from "bcrypt";
 
 // Helper function to create a token
 const createToken = (id, role) => {
-  return jwt.sign({ id, role }, process.env.JWT_SECRET_KEY, { expiresIn: '7d' });
+  return jwt.sign({ id, role }, process.env.JWT_SECRET_KEY, {
+    expiresIn: "7d",
+  });
 };
 
 // Login user
@@ -12,8 +15,8 @@ const loginUser = async (req, res) => {
 
   if (!username || !password || !role) {
     return res.status(400).json({
-      success: false, 
-      message: 'Username, password and role are required.'
+      success: false,
+      message: "Username, password and role are required",
     });
   }
 
@@ -31,24 +34,25 @@ const loginUser = async (req, res) => {
         username: username,
         id: user._id,
         role: user.role,
-        token: token
-      }
+        token: token,
+      },
     });
   } catch (err) {
     res.status(400).json({
-      success: false, 
-      message: err.message 
+      success: false,
+      message: err.message,
     });
   }
 };
-
 
 // Signup user
 const signupUser = async (req, res) => {
   const { name, username, password, role } = req.body;
 
   if (!name || !username || !password || !role) {
-    return res.status(400).json({ error: 'Name, phoneNumber, username, password, and role are required.' });
+    return res.status(400).json({
+      error: "Name, phoneNumber, username, password, and role are required.",
+    });
   }
 
   try {
@@ -58,11 +62,59 @@ const signupUser = async (req, res) => {
     // Create a token with the role
     const token = createToken(user._id, user.role);
 
-    res.status(200).json({ userid: user._id, name, username, role: user.role, token });
+    res
+      .status(200)
+      .json({ userid: user._id, name, username, role: user.role, token });
   } catch (err) {
     console.log(err);
     res.status(400).json({ error: err.message });
   }
 };
 
-export { loginUser, signupUser };
+// Change Password
+const changePassword = async (req, res) => {
+  const userId = req.user.id;
+  const { currentPassword, newPassword } = req.body;
+
+  if (!currentPassword || !newPassword) {
+    return res.status(400).json({
+      success: false,
+      message: "Current password and new password are required",
+    });
+  }
+
+  try {
+    const user = await User.findById(userId);
+    if (!user) {
+      return res
+        .status(404)
+        .json({ success: false, message: "User not found" });
+    }
+    const isPasswordValid = await bcrypt.compare(
+      currentPassword,
+      user.password
+    );
+    if (!isPasswordValid) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Current password is incorrect" });
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(newPassword, salt);
+    user.password = hash;
+    await user.save();
+
+    res.status(200).json({
+      success: true,
+      message: "Password changed successfully",
+    });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: err.message,
+    });
+  }
+};
+
+export { loginUser, signupUser, changePassword };
--- src/webApi/webRoutes/webUserRoutes.js
+++ src/webApi/webRoutes/webUserRoutes.js
@@ -1,13 +1,18 @@
-import express from 'express';
-import { loginUser, signupUser } from '../webController/webUserController.js'; // Ensure the path is correct
+import express from "express";
+import {
+  changePassword,
+  loginUser,
+  signupUser,
+} from "../webController/webUserController.js"; // Ensure the path is correct
+import requireAuth from "../../middleware/auth.js";
 
 const router = express.Router();
 
 // Route for user login
-router.post('/login', loginUser);
-
+router.post("/login", loginUser);
 // Route for user signup
-router.post('/register', signupUser);
+router.post("/register", signupUser);
+// Route for change password
+router.post("/change-password", requireAuth, changePassword);
 
 export default router;
-
